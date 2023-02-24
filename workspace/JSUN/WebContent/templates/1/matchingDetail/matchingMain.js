91861644047f1efd4504dac8ec9093ebb2b0f678
dev-taeyang <oty7942@gmail.com>
2023-02-24 18:21:57 +0900

    2/25 퍼블리싱 페이지별 분류_오태양

---
+++ workspace/JSUN/WebContent/templates/1/matchingDetail/matchingMain.js
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+
+// $(#matchingBtn).mouseover(function() {
+//     $(globalthis).css("color", "red");
+// })
+
+// document.getElementById(matchingBtn).addEventListener('mouseover', function () {
+//     this.style.color = 'red';
+// });
+
+document.getElementsByClassName(matchingBottomDiv1Div3Div1).addEventListener('mouseover', () => {
+    this.style.color = 'red';
+});
